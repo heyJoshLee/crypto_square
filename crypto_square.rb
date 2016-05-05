ec30b47f5a2fde564151eb7541d50b93b6ade273
jleewebdev <lee45276@gmail.com>
2016-05-05 20:33:27 +0800

    Four tests left

--- crypto_square.rb
+++ crypto_square.rb
@@ -1,17 +1,40 @@
 class Crypto
 
-  attr_accessor :input
+  attr_accessor :input, :size
 
   def initialize(input)
     @input = input
   end
 
-  def to_rows
-    
+  def ciphertext
+    output = ""
+    data = plaintext_segments
+
+    loop do 
+      data.each do |string|
+        output += string.slice!(0) if string.length > 0
+      end
+      break if data[0] == ""
+    end
+    output
   end
 
-  def to_coded
+  def normalize_ciphertext
 
+    ciphertext.chars.each_slice(size-1).map(&:join).join(" ")
+  end
+  
+
+  def plaintext_segments
+    normalize_plaintext.chars.each_slice(size).map(&:join)
+  end
+
+  def size
+    Math.sqrt(normalize_plaintext.length).ceil
+  end
+
+  def normalize_plaintext
+    @input = @input.gsub(/[^a-zA-Z\d]/, "").downcase
   end
 
 end
